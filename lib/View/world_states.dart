1429c6d08d58613e34c775cb2b40bbe25bb445e9
Md.Shahadat Hossain <112892447+shahadat349850@users.noreply.github.com>
2024-01-04 01:29:19 +0600

    Splash Screen has added

---
+++ lib/View/world_states.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorldStatesScreen extends StatefulWidget {
+  const WorldStatesScreen({super.key});
+
+  @override
+  State<WorldStatesScreen> createState() => _WorldStatesScreenState();
+}
+
+class _WorldStatesScreenState extends State<WorldStatesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
