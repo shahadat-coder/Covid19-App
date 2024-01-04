eb1246ce7a66b6fb86fc4b53765e50f6e8bac821
Md.Shahadat Hossain <112892447+shahadat349850@users.noreply.github.com>
2024-01-04 22:33:26 +0600

    countries screen and models has added

--- lib/View/world_states.dart
+++ lib/View/world_states.dart
@@ -1,4 +1,8 @@
+import 'package:covid_19/Model/world_states.dart';
+import 'package:covid_19/View/countries_list.dart';
+import 'package:covid_19/ViewModel/Utils/world_sates_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:pie_chart/pie_chart.dart';
 
 class WorldStatesScreen extends StatefulWidget {
@@ -8,17 +12,19 @@ class WorldStatesScreen extends StatefulWidget {
   State<WorldStatesScreen> createState() => _WorldStatesScreenState();
 }
 
-class _WorldStatesScreenState extends State<WorldStatesScreen>with TickerProviderStateMixin{
+class _WorldStatesScreenState extends State<WorldStatesScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 5), vsync: this)
+        ..repeat();
 
-  late final AnimationController _controller = AnimationController(
-      duration: const Duration(seconds: 5),
-      vsync: this)..repeat();
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     _controller.dispose();
   }
+
   final colorList = <Color>[
     const Color(0xff4285F4),
     const Color(0xff1aa260),
@@ -27,64 +33,123 @@ class _WorldStatesScreenState extends State<WorldStatesScreen>with TickerProvide
 
   @override
   Widget build(BuildContext context) {
+    StatesServices statesServices = StatesServices();
     return Scaffold(
       body: SafeArea(
-          child: Padding(padding: const EdgeInsets.all(15.0),
-              child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * .01,),
-                PieChart(
-                    dataMap:  const {
-                      "Total": 20,
-                      "Recovered": 15,
-                      "Deaths": 5,
-                    },
-                  chartRadius: MediaQuery.of(context).size.width / 3.2,
-                  legendOptions: const LegendOptions(
-                    legendPosition: LegendPosition.left,
-                  ),
-                  animationDuration: const Duration(milliseconds: 1200),
-                   chartType: ChartType.ring,
-                   colorList: colorList,
-                ),
-                Padding(
-                  padding:  EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * .06),
-                  child: Card(
-                    child: Column(
-                      children: [
-                      ReusableRow(title: "Total", value: '200'),
-                      ReusableRow(title: "Recovered", value: '150'),
-                      ReusableRow(title: "Deaths", value: '10'),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  decoration:  BoxDecoration(
-                    color: const Color(0xff1aa260),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Center(
-                    child: Text('Track Countries'),
-                  ),
-                )
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
+              ),
+              FutureBuilder(
+                  future: statesServices.fetchWorldRecords(),
+                  builder: (context, AsyncSnapshot<WorldStatesModel> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Expanded(
+                          flex: 1,
+                          child: SpinKitFadingCircle(
+                            color: Colors.white,
+                            size: 50.0,
+                            controller: _controller,
+                          ));
+                    } else {
+                      return Column(
+                        children: [
+                          PieChart(
+                            dataMap: {
+                              "Total": double.parse(
+                                  snapshot.data!.cases!.toString()),
+                              "Recovered": double.parse(
+                                  snapshot.data!.recovered.toString()),
+                              "Deaths": double.parse(
+                                  snapshot.data!.deaths.toString()),
+                            },
+                            chartValuesOptions: const ChartValuesOptions(
+                              showChartValuesInPercentage: true,
+                            ),
+                            chartRadius:
+                                MediaQuery.of(context).size.width / 3.2,
+                            legendOptions: const LegendOptions(
+                              legendPosition: LegendPosition.left,
+                            ),
+                            animationDuration:
+                                const Duration(milliseconds: 1200),
+                            chartType: ChartType.ring,
+                            colorList: colorList,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical:
+                                    MediaQuery.of(context).size.height * .06),
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  ReusableRow(
+                                      title: 'Total Cases',
+                                      value: snapshot.data!.cases.toString()),
+                                  ReusableRow(
+                                      title: 'Deaths',
+                                      value: snapshot.data!.deaths.toString()),
+                                  ReusableRow(
+                                      title: 'Recovered',
+                                      value:
+                                          snapshot.data!.recovered.toString()),
+                                  ReusableRow(
+                                      title: 'Active',
+                                      value: snapshot.data!.active.toString()),
+                                  ReusableRow(
+                                      title: 'Critical',
+                                      value:
+                                          snapshot.data!.critical.toString()),
+                                  ReusableRow(
+                                      title: 'Today Deaths',
+                                      value: snapshot.data!.todayDeaths
+                                          .toString()),
+                                  ReusableRow(
+                                      title: 'Today Recovered',
+                                      value: snapshot.data!.todayRecovered
+                                          .toString()),
+                                ],
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap:(){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> const CountriesList()));                            },
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: const Color(0xff1aa260),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const Center(
+                                child: Text('Track Countries'),
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }
+                  }),
             ],
           ),
-          ),
-
+        ),
       ),
     );
   }
 }
+
 class ReusableRow extends StatelessWidget {
-  String title,value;
-   ReusableRow({super.key,required this.title,required this.value});
+  String title, value;
+
+  ReusableRow({super.key, required this.title, required this.value});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10,right: 10,top: 10,bottom: 5),
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 5),
       child: Column(
         children: [
           Row(
@@ -94,7 +159,9 @@ class ReusableRow extends StatelessWidget {
               Text(value),
             ],
           ),
-          const SizedBox(height: 5,),
+          const SizedBox(
+            height: 5,
+          ),
           const Divider(),
         ],
       ),
